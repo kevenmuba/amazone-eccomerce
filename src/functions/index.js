85417946c1c48b02bf35f2ca35f8d75a8316545e
kevenmuba <kevk0758@gmail.com>
2024-07-12 11:17:07 -0700

    setting up the function

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,19 +1,19 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();// this reads the env file 
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const app = express();
+app.use(cors({ origin: true }));
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+app.use(express.json());
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success",
+  });
+});
+
+exports.api = onRequest(app);// this is listening using firebase
