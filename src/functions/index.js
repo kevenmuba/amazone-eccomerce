308b2a2b7a04bb63138849471d895484f22b8f02
kevenmuba <kevk0758@gmail.com>
2024-07-12 13:41:35 -0700

    amazone backend

--- src/functions/index.js
+++ src/functions/index.js
@@ -8,12 +8,31 @@ const stripe = require("stripe")(process.env.STRIPE_KEY);
 const app = express();
 app.use(cors({ origin: true }));
 
-app.use(express.json());
+app.use(express.json());//we are saying  you can read json 
 
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Success",
   });
 });
+
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    //console.log(paymentIntent); paymentINtent stripachin clientu silk be object melk buzu neger tiyizal keza wust wanaw gn client_secret new yachi keteleyayu api gar yemnlkbet new
+    res.status(201).json({
+      clientSecret:paymentIntent.client_secret,
+    });
+    // hidetu backendachin le react clent secretachin yilkal then react degmo be stripe confirm endidereglet yachin  client_secret yitekemal 
+  } else {
+    res.status(403).json({
+      message: "total payment must be greater than 0",
+    });
+  }
+})
 
 exports.api = onRequest(app);// this is listening using firebase
